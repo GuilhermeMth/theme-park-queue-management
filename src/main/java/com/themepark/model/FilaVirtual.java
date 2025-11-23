79b6e4ef1a70e26c88af8edcae91045e037587ff
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-11-23 12:17:17 -0300

    feat: add getNivelprioridade in enum

--- src/main/java/com/themepark/model/FilaVirtual.java
+++ src/main/java/com/themepark/model/FilaVirtual.java
@@ -1,4 +1,67 @@
 package com.themepark.model;
 
+import com.themepark.model.datastructures.LinkedList;
+
+import java.time.LocalTime;
+
 public class FilaVirtual {
+    private LinkedList<Visitante> fila;
+    private Atracao atracao;
+    private int tempoEsperaMinutos;
+    private LocalTime ultimaSessao;
+
+    public FilaVirtual(Atracao atracao) {
+        this.atracao = atracao;
+        this.fila = new LinkedList<>();
+        this.tempoEsperaMinutos = 0;
+        this.ultimaSessao = LocalTime.now();
+    }
+
+    public void addVisitante(Visitante visitante) {
+
+    }
+
+    public Atracao getAtracao() {
+        return atracao;
+    }
+
+    public void setAtracao(Atracao atracao) {
+        this.atracao = atracao;
+    }
+
+    public LinkedList<Visitante> getFila() {
+        return fila;
+    }
+
+    public void setFila(LinkedList<Visitante> fila) {
+        this.fila = fila;
+    }
+
+    public int getTempoEsperaMinutos() {
+        return tempoEsperaMinutos;
+    }
+
+    public void setTempoEsperaMinutos(int tempoEsperaMinutos) {
+        this.tempoEsperaMinutos = tempoEsperaMinutos;
+    }
+
+    public LocalTime getUltimaSessao() {
+        return ultimaSessao;
+    }
+
+    public void setUltimaSessao(LocalTime ultimaSessao) {
+        this.ultimaSessao = ultimaSessao;
+    }
+
+    @Override
+    public String toString() {
+        return "FilaVirtual{" +
+                "atracao=" + atracao +
+                ", fila=" + fila +
+                ", tempoEsperaMinutos=" + tempoEsperaMinutos +
+                ", ultimaSessao=" + ultimaSessao +
+                '}';
+    }
+
+
 }
--- src/main/java/com/themepark/model/TipoIngresso.java
+++ src/main/java/com/themepark/model/TipoIngresso.java
@@ -1,7 +1,17 @@
 package com.themepark.model;
 
 public enum TipoIngresso {
-    ELITE,
-    PREMIUM,
-    COMUM
+    COMUM(1),
+    PREMIUM(2),
+    ELITE(3);
+
+    private final int nivelPrioridade;
+
+    TipoIngresso(int nivelPrioridade) {
+        this.nivelPrioridade = nivelPrioridade;
+    }
+
+    public int getNivelPrioridade() {
+        return nivelPrioridade;
+    }
 }
